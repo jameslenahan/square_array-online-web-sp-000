fce1aa3082bc6a011945952cf67687cf0c51f4e6
James Lenahan <james.k.lenahan@gmail.com>
2019-11-27 15:03:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,8 @@
 def square_array(array)
-  square_numbers = []
-  array.each{|a| array.push(a*a)}
-  return array
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
 end
+
+arr = [1, 2, 3, 4, 5]
+square_array(arr)
